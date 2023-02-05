4b94c8e709fd915f5b564f1eca47114685877186
coder_J <suh4joy@gmail.com>
2023-02-04 18:16:57 -0600

    added the first foundation block

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameBoardComponent } from './game-board/game-board.component';
+import { BrickComponent } from './brick/brick.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameBoardComponent
+    GameBoardComponent,
+    BrickComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/brick/brick.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-brick',
+  templateUrl: './brick.component.html',
+  styleUrls: ['./brick.component.scss']
+})
+export class BrickComponent {
+
+}
